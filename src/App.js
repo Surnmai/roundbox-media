42861ab7c50dd84b02d49cd0f3ada8b7cd6b577b
Surnmai <surnmai1@gmail.com>
2023-12-03 09:50:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./img/roundbox.png";
+
+// import { Link } from "react-router-dom";
+import { FaInstagram } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="bg-hero h-[785px]  w-full bg-cover bg-no-repeat bg-center relative">
+        <div className="container">
+          {/* title and logo  */}
+          <div className="flex items-center justify-between ">
+            {/* title  */}
+            <h3 className="text-primary max-h-9 font-poppins max-w-xl font-bold text-3xl ">
+              Production management, event coverage and media support to suit
+              your organization
+            </h3>
+
+            {/* logo  */}
+            <a className="cursor-pointer item">
+              <img src={Logo} alt="logo" className="w-[10rem] " />
+            </a>
+          </div>
+
+          {/* text and contact  */}
+          <div className="flex justify-between mt-24 text-white font-dmSans">
+            {/* text  */}
+            <div className="capitalize  max-w-xl">
+              <div className="text-4xl font-bold mb-6">
+                <p>production management</p>
+                <p>Videography/photography</p>
+                <p>Live event streaming</p>
+                <p>Live worship</p>
+                <p>Church Media Consultancy</p>
+              </div>
+              <article className="text-2xl">
+                From worship, to concerts, to grand openings, to corporate
+                meetings, Roundbox.media is here to help
+              </article>
+            </div>
+
+            {/* contact  */}
+
+            <div className="rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+              <div className=" flex flex-col justify-center h-full w-full bg-transparent font-bold font-dmSans text-6xl p-6">
+                <p>055 847 1720</p>
+                <p>050 843 4937</p>
+                <div className="flex">
+                  <FaInstagram />
+                  <span>roundbox.media</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* gradiant border  */}
+          <div className="translate-y-32 rounded-md h-10 w-full bg-gradient-to-r from-black from-30% via-red-800 via-60% to-sky-500 to-90%"></div>
+
+          {/* bottom text  */}
+          <div className="flex justify-between translate-y-[8rem] text-2xl">
+            <p className="text-primary">site under development </p>
+            <p className="text-white">.goodstuff </p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // IT AFFECTS THE HORIZONTAL PADDING  FOR THE SCREEN SIZES
+    // screens: {
+    //   sm: "480px",
+    //   md: "768px",
+    //   lg: "1024px",
+    // },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+      },
+    },
+    fontFamily: {
+      dmSans: ["DM Sans", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        hero: "url('/src/img/BG2-only.jpg')",
+      },
+      colors: {
+        primary: "#fcd42f",
+      },
+    },
+  },
+  plugins: [],
+};
