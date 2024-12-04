a204be3f038feffa9ee31d2e80205c84b338c350
Surnmai <surnmai1@gmail.com>
2024-12-04 11:56:43 +0000

    img height

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { FaInstagram } from "react-icons/fa";
 function App() {
   return (
     <>
-      <section className="bg-hero h-[785px]  w-full bg-cover bg-no-repeat bg-center relative">
+      <section className="bg-hero h-[46.9rem] w-full bg-cover bg-no-repeat bg-center relative">
         <div className="container">
           {/* title and logo  */}
           <div className="flex items-center justify-between ">
             {/* title  */}
+
             <h3 className="text-primary max-h-9 font-poppins max-w-xl font-bold text-3xl ">
               Production management, event coverage and media support to suit
               your organization
@@ -22,10 +23,11 @@ function App() {
             </div>
           </div>
 
+          {/* add flex basis, shrink and grow to the children  */}
           {/* text and contact  */}
-          <div className="flex justify-between mt-24 text-white font-dmSans">
+          <div className="flex justify-between items-center mt-24 text-white font-dmSans">
             {/* text  */}
-            <div className="capitalize  max-w-xl">
+            <div className="flex-1 capitalize  max-w-xl">
               <div className="text-4xl font-bold mb-6">
                 <p>production management</p>
                 <p>Videography/photography</p>
@@ -40,9 +42,8 @@ function App() {
             </div>
 
             {/* contact  */}
-
-            <div className="rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
-              <div className=" flex flex-col justify-center h-full w-full bg-transparent font-bold font-dmSans text-6xl p-6">
+            <div className="flex-1 rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+              <div className=" flex flex-col justify-center items-center h-[20rem] w-full bg-transparent font-bold font-dmSans text-6xl p-6">
                 <p>055 847 1720</p>
                 <p>050 843 4937</p>
                 <div className="flex">
@@ -54,7 +55,7 @@ function App() {
           </div>
 
           {/* gradiant border  */}
-          <div className="translate-y-32 rounded-md h-10 w-full bg-gradient-to-r from-black from-30% via-red-800 via-60% to-sky-500 to-90%"></div>
+          {/* <div className="translate-y-32 rounded-md h-10 w-full bg-gradient-to-r from-black from-30% via-red-800 via-60% to-sky-500 to-90%"></div> */}
 
           {/* bottom text  */}
           <div className="flex justify-between translate-y-[8rem] text-2xl">
